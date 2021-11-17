c90a54ad7815218d3ad14b897339888afabc445e
ghajduk3 <gh8590@student.uni-lj.si>
2021-11-17 21:20:29 +0100

    Init commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # roche-back
+
+link: https://dev.to/nobleobioma/create-a-simple-rest-api-with-django-253p 
---
+++ roche_back/roche_api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+
+@api_view(["GET"])
+def welcome(request):
+    content = {"message": "Welcome to the roche back!"}
+    return JsonResponse(content)
