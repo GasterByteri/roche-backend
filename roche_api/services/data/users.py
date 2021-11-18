0679a9122c625f441325488e2e341dbecc4d3a9b
ghajduk3 <gh8590@student.uni-lj.si>
2021-11-18 20:31:42 +0100

    Fixed registering different user profiles based on the user role

--- roche_api/services/data/users.py
+++ roche_api/services/data/users.py
@@ -4,22 +4,20 @@ from roche_api.models import users as user_models
 
 def create_user_profile(request_post: dict) -> None:
     if request_post.get("role") == api_constants.PATIENT:
-        patient_data = {
-            "municipality" : request_post.get("municipality", ""),
-            "medical_record_number" : request_post.get("medical_record_number", ""),
-            "diagnosis" : request_post.get("diagnosis", ""),
-        }
-        patient = user_models.Patient(patient_data)
+        patient = user_models.Patient(
+            municipality=request_post.get("municipality", ""),
+            medical_record_number=request_post.get("medical_record_number", ""),
+            diagnosis=request_post.get("diagnosis", ""),
+        )
         return patient
 
     elif request_post.get("role") == api_constants.DOCTOR:
-        doctor_data = {
-                            "employee_number": request_post.get("employee_number", ""),
-                            "department": request_post.get("department", ""),
-                            "title": request_post.get("title", ""),
-                            "room_number": request_post.get("room_number"),
-                            "hospital": request_post.get("hospital", ""),
-                            "city": request_post.get("city", ""),
-                       }
-        doctor = user_models.Doctor(doctor_data)
+        doctor = user_models.Doctor(
+            employee_number=request_post.get("employee_number", ""),
+            department=request_post.get("department", ""),
+            title=request_post.get("title", ""),
+            room_number=request_post.get("room_number"),
+            hospital=request_post.get("hospital", ""),
+            city=request_post.get("city", ""),
+        )
         return doctor
