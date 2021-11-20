f64aa943c744f48be11966954b01c559f87060de
ghajduk3 <gh8590@student.uni-lj.si>
2021-11-20 12:20:23 +0100

    Chat client env

--- roche_api/services/chat.py
+++ roche_api/services/chat.py
@@ -1,11 +1,16 @@
 from rocketchat.api import RocketChatAPI
 import requests
 import os
+import environ
+
+# Reading environment variables defined in .env
+env = environ.Env()
+environ.Env.read_env()
 
 class ChatClient:
     api_host = 'http://23.88.57.2:3000'
     api_endpoint = os.path.join(api_host, "api/v1/")
-    api_client = RocketChatAPI(settings={'username': 'ghajduk3','password': 'hackaton2021','domain': 'http://23.88.57.2:3000'})
+    api_client = RocketChatAPI(settings={'username': env('ROCKET_CHAT_USERNAME'),'password': env('ROCKET_CHAT_PASS'),'domain': api_host})
 
     def create_user(self, user):
         chat_user_data = {
